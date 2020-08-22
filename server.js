2e17aa41f7ce25a885836ec790d6512f06b85aa4
Fenix D Rajwin <mailtofenixd@gmail.com>
2020-08-22 12:22:55 +0530

    Server setup

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//Connects server to DB through mongoose
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running...'));
 
