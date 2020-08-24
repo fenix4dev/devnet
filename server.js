44fdc027f8a66a4f7e52e28f7ba36ae25dc859b5
Fenix D Rajwin <mailtofenixd@gmail.com>
2020-08-24 23:40:17 +0530

    Routes, Middleware and JWT Auth

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connects server to DB through mongoose
 connectDB();
 
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running...'));
 
 //Define Routes
