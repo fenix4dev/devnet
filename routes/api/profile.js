921eb5e671ca9ef6777eb5538c7b5959a02d91e8
Fenix D Rajwin <mailtofenixd@gmail.com>
2020-11-01 00:11:39 +0530

    Dashboard and Profile Management Final Commit

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,7 @@ const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route   GET api/profile/me
 //@desc     Get current user profile
@@ -152,8 +153,8 @@ router.get('/user/:user_id', async (req, res) => {
 
 router.delete('/', auth, async (req, res) => {
   try {
-    // @todo - remove users posts
-
+    //Remove user Posts
+    await Post.deleteMany({ user: req.user.id });
     // Remove Profile
     await Profile.findOneAndRemove({ user: req.user.id });
     // Remove User
